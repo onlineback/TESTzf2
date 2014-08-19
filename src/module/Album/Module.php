7642e8a10638bf456dc3b654e2ee7a33c4802f85
Marcelo Carranza <marrselo@gmail.com>
2014-08-19 18:31:37 -0500

    construyendo satelital zend2

---
+++ src/module/Album/Module.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ namespace Album;
+
+ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+ use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+ {
+     public function getAutoloaderConfig()
+     {
+         return array(
+             'Zend\Loader\ClassMapAutoloader' => array(
+                 __DIR__ . '/autoload_classmap.php',
+             ),
+             'Zend\Loader\StandardAutoloader' => array(
+                 'namespaces' => array(
+                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                 ),
+             ),
+         );
+     }
+
+     public function getConfig()
+     {
+         return include __DIR__ . '/config/module.config.php';
+     }
+ }
---
+++ src/module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,35 @@
+<?php
+ namespace Album\Controller;
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AlbumController
+ *
+ * @author marrselo
+ */
+class AlbumController extends AbstractActionController {
+    //put your code here
+   // dominios@punto.pe
+    public function indexAction()
+    {
+//        Album\Controller\AlbumController::indexAction;
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
